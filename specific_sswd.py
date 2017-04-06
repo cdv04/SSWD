adbb5b5ad5188b0870739129416e2fa6f6288b19
Zackary BEAUGELIN <zackary.bzeaugelin@epitech.eu>
2017-04-06 11:20:52 +0200

    First save, Collection & ponderation done

---
+++ specific_sswd.py
@@ -0,0 +1,33 @@
+"""File use to run the program without ACT module (from excel?)."""
+
+# @Author: Zackary BEAUGELIN <gysco>
+# @Date:   2017-04-05T09:54:15+02:00
+# @Email:  zackary.beaugelin@epitech.eu
+# @Project: SSWD
+# @Filename: specific_sswd.py
+# @Last modified by:   gysco
+# @Last modified time: 2017-04-05T10:15:13+02:00
+
+
+def ajoute_series_act(nom_feuille, nseries, ligne_data,
+                      col_tax, col_data, col_pcum):
+    """Empty function use to run tracer_graphique without ACT module."""
+    pass
+
+
+def init_collection_act(nom_feuille, l_espece, l_taxo, l_data, l_test,
+                        c_espece, c_taxo, c_data, c_test, data_co, nom_ve,
+                        nom_inv, nom_al, nom_testC, nom_testA):
+    """Empty function use to run charger_parametres without ACT module."""
+    pass
+
+
+def filtre_collection_act(data_co, nom1, nom2, nom_testA, nom_al, nb_vea,
+                          nb_inva, erreur):
+    """Empty function use to run charger_parametres without ACT module."""
+    pass
+
+
+def change_nom_taxo(data_co):
+    """Empty function use to run charger_parametres without ACT module."""
+    pass
