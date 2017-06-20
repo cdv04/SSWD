3340e3a2f5d0442c95927f6ebdd09c41e9c1ea1a
Gysco <zackary.beaugelin@epitech.eu>
2017-06-20 14:00:26 +0200

    Update build appveyor and travis

---
+++ .travis/install.sh
@@ -0,0 +1,24 @@
+# @Author: Zackary BEAUGELIN <gysco>
+# @Date:   2017-06-20T13:17:52+02:00
+# @Email:  zackary.b@live.fr
+# @Project: PyMENT-SSWD
+# @Filename: install.sh
+# @Last modified by:   gysco
+# @Last modified time: 2017-06-20T13:55:45+02:00
+
+#!/bin/bash
+
+if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+  brew install wxmac wxpython
+else
+  apt-get install ibwebkitgtk-dev, libjpeg-dev, libtiff-dev, libgtk2.0-dev, libsdl1.2-dev, libgstreamer-plugins-base0.10-dev, freeglut3, freeglut3-dev, libnotify-dev, wx-common
+fi
+pip install -r requirements.txt
+if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+  pyinstaller pyment-sswd_mac.spec -n pyment-sswd_mac --distpath=./dist/mac
+  hdiutil create dist/pyment-sswd_mac.dmg -srcfolder dist/mac/ -ov
+  zip -r dist/pyment-sswd_mac.zip dist/mac/pyment-sswd_mac.app
+else
+  pyinstaller pyment/__main__.py -w -n pyment-sswd_unix --distpath=./dist/unix
+  tar -xcvf dist/pyment-sswd_unix.tar.gz dist/unix/
+fi
