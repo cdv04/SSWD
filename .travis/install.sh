81c907ce10d01b2c246ecc971895fbdad2cf4809
Gysco <zackary.beaugelin@epitech.eu>
2017-06-20 16:24:12 +0200

    Update Appveyor Python Version & Travis Root

--- .travis/install.sh
+++ .travis/install.sh
@@ -4,14 +4,12 @@
 # @Project: PyMENT-SSWD
 # @Filename: install.sh
 # @Last modified by:   gysco
-# @Last modified time: 2017-06-20T15:01:36+02:00
+# @Last modified time: 2017-06-20T16:33:14+02:00
 
 #!/bin/bash
 
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
   brew install wxmac wxpython
-else
-  sudo apt-get install ibwebkitgtk-dev libjpeg-dev libtiff-dev libgtk2.0-dev libsdl1.2-dev libgstreamer-plugins-base0.10-dev freeglut3 freeglut3-dev libnotify-dev wx-common
 fi
 pip install -r requirements.txt
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
