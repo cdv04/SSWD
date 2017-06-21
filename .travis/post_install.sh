a892e40e9e2f8a8b9b46736d89d935ad6c39e2c7
Gysco <zackary.beaugelin@epitech.eu>
2017-06-21 14:10:09 +0200

    Build off for appveyor

--- .travis/post_install.sh
+++ .travis/post_install.sh
@@ -12,7 +12,8 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
   hdiutil create dist/pyment-sswd_mac.dmg -srcfolder dist/mac/ -ov
   zip -r dist/pyment-sswd_mac.zip dist/mac/pyment-sswd_mac.app
 else
-  LD_PRELOAD=/opt/$(python3 --version | awk '{ gsub (" ", "", $0); print}')/lib/python3.5/site-packages/wx/libwx_gtk2u_core-3.0.so.0 python3 -c "import wx; print(wx.__version__)"
-  LD_PRELOAD=/opt/$(python3 --version | awk '{ gsub (" ", "", $0); print}')/lib/python3.5/site-packages/wx/libwx_gtk2u_core-3.0.so.0 pyinstaller pyment/__main__.py -w -n pyment-sswd_unix --distpath=./dist/unix
+  ls -l /opt/$(python3 --version | awk '{ gsub (" ", "", $0); print}')/lib/python3.5/site-packages/wx/libwx_gtk2u_core-3.0.so
+  LD_PRELOAD=/opt/$(python3 --version | awk '{ gsub (" ", "", $0); print}')/lib/python3.5/site-packages/wx/libwx_gtk2u_core-3.0.so python3 -c "import wx; print(wx.__version__)"
+  LD_PRELOAD=/opt/$(python3 --version | awk '{ gsub (" ", "", $0); print}')/lib/python3.5/site-packages/wx/libwx_gtk2u_core-3.0.so pyinstaller pyment/__main__.py -w -n pyment-sswd_unix --distpath=./dist/unix
   zip -r dist/pyment-sswd_unix.zip dist/unix/pyment-sswd_unix
 fi
